37a26547da0c217528e008ad0f406cbede12efc7
Mike McGrath <mike.mcgrath@rallysportdirect.com>
2020-05-31 03:09:28 -0600

    init

---
+++ index.js
@@ -0,0 +1,63 @@
+const { BotFrameworkAdapter, ConversationState, InputHints, MemoryStorage, UserState, ActivityTypes } = require('botbuilder');
+
+// Import required packages
+const path = require('path');
+const restify = require('restify');
+
+// Note: Microsoft Stuffs
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_SECRET
+});
+
+
+// For local development, in-memory storage is used.
+// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
+// is restarted, anything stored in memory will be gone.
+const memoryStorage = new MemoryStorage();
+const conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage);
+
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+
+// Listen for incoming activities and route them to your bot main dialog.
+server.post('/api/messages', (req, res) => {
+    // Route received a request to adapter for processing
+    adapter.processActivity(req, res, async (context) => {
+        if (context.activity.type === 'message') {
+            conversationState.get(context);
+            await context.sendActivity(`You Said: ${context.activity.text}`);
+        } else {
+            await context.sendActivity(`${context.activity.type}`);
+        }
+    });
+});
+
+// Listen for Upgrade requests for Streaming.
+server.on('upgrade', (req, socket, head) => {
+    // Create an adapter scoped to this WebSocket connection to allow storing session data.
+    const streamingAdapter = new BotFrameworkAdapter({
+        appId: process.env.APP_ID,
+        appPassword: process.env.APP_SECRET
+    });
+    // Set onTurnError for the BotFrameworkAdapter created for each connection.
+    streamingAdapter.onTurnError = onTurnErrorHandler;
+
+    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
+        // After connecting via WebSocket, run this logic for every request sent over
+        // the WebSocket connection.
+        //await bot.run(context);
+    });
+});
